38b1e6aa90341ded03025785d46409418537b39e
Ben Huson <ben@thewhiteroom.net>
2012-05-02 15:22:24 +0100

    Default to center alignment.

--- core/js/supersized.core.3.2.1.js
+++ core/js/supersized.core.3.2.1.js
@@ -292,7 +292,7 @@
 		min_height		        :   0,			// Min height allowed (in pixels)
 		vertical_center         :   1,			// Vertically center background
 		horizontal_center       :   1,			// Horizontally center background
-		horizontal_align        :   'left',		// Horizontally align background
+		horizontal_align        :   'center',	// Horizontally align background
 		fit_always				:	0,			// Image will never exceed browser width or height (Ignores min. dimensions)
 		fit_portrait         	:   1,			// Portrait images will not exceed browser height
 		fit_landscape			:   0			// Landscape images will not exceed browser width
