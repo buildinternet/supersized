a3f35243f7b0a29c0d2b990bbf2ab9e55a8a7d8d
Matt Richardson <matt@redgumtech.com.au>
2012-06-17 17:17:06 +0100

    Bug fix for data.photoset.owner null
    A previous change (pull request #66) appears to have introduced a bug
    for user based photos, while fixing a bug for set based photos. This now
    uses data.photoset.owner when data.photoset exists, otherwise, reverts
    to item.owner.

--- flickr/js/supersized.flickr.1.1.2.js
+++ flickr/js/supersized.flickr.1.1.2.js
@@ -167,7 +167,7 @@
     			    //create image urls
     			    var photoURL = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_' + options.image_size + '.jpg';
     			    var thumbURL = 'http://farm' + item.farm + '.static.flickr.com/' + item.server + '/' + item.id + '_' + item.secret + '_t.jpg';
-    			   	var	photoLink = "http://www.flickr.com/photos/" + data.photoset.owner + "/" + item.id + "/";
+    			    var photoLink = "http://www.flickr.com/photos/" + (data.photoset ? data.photoset.owner : item.owner) + "/" + item.id + "/";
     			   	
     			    if (i == 0){
     			    	options.slides.splice(0,1,{ image : photoURL, thumb : thumbURL, title : item.title , url : photoLink });
